f4f8eaade0b73241f409f5e1bcc802eeb06f3868
Clara Fonte <clara00017@hotmail.com>
2023-10-05 20:02:56 -0300

    organização de pastas e Entidade Usuário

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,7 +2,10 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.paciente.*;
+import med.voll.api.domain.paciente.DadosListagemPaciente;
+import med.voll.api.domain.paciente.Paciente;
+import med.voll.api.domain.paciente.PacienteRepository;
+import med.voll.api.domain.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,7 +23,7 @@ public class PacienteController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity<DadosDetalhamentoPaciente> cadastrar(@RequestBody @Valid DadosCadastrosPacientes dados, UriComponentsBuilder uriBuilder){
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastrosPacientes dados, UriComponentsBuilder uriBuilder){
 
         var paciente = new Paciente(dados);
         repository.save(paciente);
